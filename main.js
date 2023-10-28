03692c8a21dc9d20a7304e814c22bf8f0fbeda68
kaunbk <128196667+kaunbk@users.noreply.github.com>
2023-10-27 21:55:25 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+function tocaSomPom(){
+  document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap(){
+document.querySelector('#som_tecla_clap').play();
+}
+
+function tocaSom(idElementoAudio){
+document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+for(let contador=0; contador <listaDeTeclas.length; contador++) {
+  const tecla= listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio =`#som_${instrumento}`;
+  tecla.onclick =function () {  
+  tocaSom(idAudio);
+};
+
+  tecla.onkeydown =function (evento) {
+  if (evento.code === 'Enter') {
+  tecla.classList.add('ativa');
+  }
+  console.log(evento.code);
+}
+  
+  tecla.onkeyup =function () {
+  tecla.classList.remove('ativa');
+}
+  
+  console.log(instrumento);
+}
